f34b095a68be425251e5739aa4bc109934f6b71a
Virginia Herron <inadream@gmail.com>
2018-01-21 04:26:16 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,14 +1,15 @@
 def turn(board)
+  stop = false
   loop do
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
   if index == valid_move?(board,index)
-    true
+    stop = true
   else
     false
   end
-  if true
+  if stop = true
     move(board,index,value = "X")
     break
   end
